8c6d3e386ab5d09c1ec178b4770cd82a903765d1
Duc DT <trongduc868@gmail.com>
2021-06-12 13:31:38 +0700

    update css

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -92,10 +92,6 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-                            <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs ">
                                 {{ __('Manage Accounts') }}
                             </div>
